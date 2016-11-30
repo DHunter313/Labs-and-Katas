f351ee411d6395a0659630cefe1c233ac1eb1a9c
Darius Hunter <darius777hunter@gmail.com>
2016-11-30 15:57:11 -0500

    Revert "Revert "ReOrganized GitHub""
    This reverts commit d691e338515680be10cf2a7e1673653d4307bdd0.

---
+++ Lab1/Completed lab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Completed_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Continue = true;
+            do
+            {
+
+                Console.WriteLine("Welcome to Grand Circus' Room Detail Generator");
+                Console.WriteLine();
+                Console.WriteLine("Enter Length");
+                string length = Console.ReadLine();
+                double Value1 = double.Parse(length);
+                Console.WriteLine("Enter Width");
+                string width = Console.ReadLine();
+                double Value2 = double.Parse(width);
+
+                Console.WriteLine("Area:");
+                double Area = Value1 * Value2;
+                Console.WriteLine(Area);
+                Console.WriteLine("Perimeter");
+                double Perimeter = 2 * (Value1 * Value2);
+                Console.WriteLine(Perimeter);
+                Console.WriteLine();
+                Console.WriteLine("Continue (y/n)");
+                string response = Console.ReadLine();
+                Continue = response == "y";
+            } while (Continue);
+
+
+
+
+        }
+    }
+}
---
+++ Lab10MovieListApp/MovieAppList/MovieAppList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MovieAppList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cont;
+            Console.WriteLine("Welcome to the Movie List Application\n\n\n");
+            Console.WriteLine("There are 100 movies in this list\n\n");
+
+            do
+            {
+                Console.Clear();
+            Console.WriteLine( "The categories are: comedy, drama, musical, scifi, horror, animated\n\n");
+
+                Console.Write("What category are you interested in?: ");
+                string movieCategory = Console.ReadLine();
+                Console.WriteLine();
+                List<Movie> Catalog = MovieList.default_MovieCatalog;
+
+                foreach (Movie movie in Catalog)
+                {
+                    if (movie.category == movieCategory)
+                    {
+                        Console.WriteLine(movie.title);
+
+                    }
+
+                }
+
+                Console.WriteLine("\n\nWould you like to search again? enter 'y' or 'n'");
+                cont = Console.ReadLine();
+            } while (cont == "y" || cont == "Y");
+        }
+    }
+}
